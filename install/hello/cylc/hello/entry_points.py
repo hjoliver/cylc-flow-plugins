a61539e1d59d4e4ac316f5c30fd3eabbf5581cde
Hilary James Oliver <hilary.j.oliver@gmail.com>
2022-06-03 10:00:39 +1200

    Fix plugin return values.

--- install/hello/cylc/hello/entry_points.py
+++ install/hello/cylc/hello/entry_points.py
@@ -7,7 +7,13 @@ from cylc.hello.utils import speak
 
 def pre_configure(srcdir=None, opts=None, rundir=None):
     speak("pre_configure", srcdir, rundir)
+    return {
+        "env": {},
+        "template_variables": {},
+        "templating_detected": None
+    }
 
 
 def post_install(srcdir=None, opts=None, rundir=None):
     speak("post_install", srcdir, rundir)
+    return True
